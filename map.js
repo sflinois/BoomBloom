b1d78aa26f56dcb2b0b981a50d9c0de3c214f71d
sflinois <sflinois@student.42.fr>
2019-10-12 17:30:19 +0200

    map working

--- map.js
+++ map.js
@@ -1,8 +1,15 @@
 // var mymap = L.map('mapid').setView([48.6739267, -3.5871429], 13);
-var mymap = L.map('mapid').setView([48.6739267, -3.5871429], 13);
+var mymap = L.map('mapid', {zoomControl: false}).setView([48.6739267, -3.5871429], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox.streets',
 	accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
+
+mymap.dragging.disable();
+mymap.touchZoom.disable();
+mymap.doubleClickZoom.disable();
+mymap.scrollWheelZoom.disable();
+mymap.boxZoom.disable();
+mymap.keyboard.disable();
