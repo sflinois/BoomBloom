bb3d8a66c31961da4d1ef857b38ab140f92f29ec
Pepito <benoit.messiaen@ensg.eu>
2019-10-13 13:47:06 +0200

    f

--- charts.js
+++ charts.js
@@ -1,21 +1,10 @@
-function renderChart(data, labels, name) {
+function renderChart(datasets, labels, name) {
     var ctx = document.getElementById(name).getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: labels,
-            datasets: [{
-                label: 'Indice NDVI',
-                data: data.ndvi,
-                backgroundColor: 'rgba(50, 205, 50, 0.2)',
-                borderWidth: 1
-            },
-            {
-                label: 'Indice Infra',
-                data: data.infra,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderWidth: 1
-            },]
+            labels,
+            datasets
         },
     });
 }
--- map.js
+++ map.js
@@ -1,6 +1,9 @@
 // var mymap = L.map('mapid').setView([48.6739267, -3.5871429], 13);
 var mymap = L.map('mapid', {zoomControl: false}).setView([48.6837671, -3.5896207], 14);
-L.tileLayer.wms('https://geobretagne.fr/geoserver/ign/ows?', {
+let phototile = L.tileLayer.wms('https://geobretagne.fr/geoserver/photo/wms?', {
+    layers: 'WMS:ortho-22-2015'
+});
+let osmtile = L.tileLayer.wms('https://geobretagne.fr/geoserver/ign/ows?', {
     layers: 'WMS:scan_littoral'
 }).addTo(mymap);
 
